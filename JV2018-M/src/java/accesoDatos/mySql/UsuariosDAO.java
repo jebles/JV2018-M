eb813868a8b63f3b70acfe330e0081bf7d69de5d
Jesús Pérez Robles <4004010@alu.murciaeduca.es>
2019-05-28 10:08:27 +0200

    cambios UsuarioDAO sql II

--- JV2018-M/src/java/accesoDatos/mySql/UsuariosDAO.java
+++ JV2018-M/src/java/accesoDatos/mySql/UsuariosDAO.java
@@ -11,10 +11,8 @@
 
 package accesoDatos.mySql;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.table.DefaultTableModel;
@@ -25,17 +23,21 @@ import config.Configuracion;
 import modelo.*;
 import modelo.Usuario.RolUsuario;
 import util.Fecha;
+import util.Formato;
 
 
 public class UsuariosDAO  implements OperacionesDAO {
 
 	// Singleton. 
-	private static UsuariosDAO instance;
+	private static UsuariosDAO instance = null;
 
 	// Base datos mySQL
 	private Connection db;
-	private Statement sentenciaUsr;
-	private Statement sentenciaId;
+	private Statement stUsuarios;
+	private ResultSet rsUsuarios;
+	private DefaultTableModel tmUsuarios;
+	private ArrayList<Usuario> bufferUsuarios;
+	// private Statement sentenciaId;
 	// private DefaultTableModel 
 
 	/**
@@ -51,13 +53,13 @@ public class UsuariosDAO  implements OperacionesDAO {
 		}
 		return instance;
 	}
-	
+
 	/**
 	 * Constructor por defecto de uso interno.
 	 * Sólo se ejecutará una vez.
 	 */
 	private UsuariosDAO() {
-		db= Conexion.getDB();
+		//db= Conexion.getDB();
 		try {
 			inicializar();			
 		}
@@ -69,37 +71,38 @@ public class UsuariosDAO  implements OperacionesDAO {
 		}
 	}
 
-	
+
 
 	private void inicializar() throws SQLException {
-		// db= Conexion.getDB();
+		db= Conexion.getDB();
 		try {
-			sentenciaUsr = db.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-			sentenciaUsr.executeQuery("select * from usuarios");
+			crearTablaUsuarios();
+			this.stUsuarios = db.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			//sentenciaUsr.executeQuery("select * from usuarios");
 		}
 		catch (SQLException e) {
 			crearTablaUsuarios();
 		}
-		
-		try {
-			sentenciaId = db.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-			sentenciaId.executeQuery("select * from equivalId");
-			
-		}
-		catch (SQLException e) {
-			crearTablaEquivalId();
-		}
+
+		//		try {
+		//			sentenciaId = db.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+		//			sentenciaId.executeQuery("select * from usuarios where ");
+		//			
+		//		}
+		//		catch (SQLException e) {
+		//			crearTablaEquivalId();
+		//		}
 	}
 
 	private void crearTablaEquivalId() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void crearTablaUsuarios() throws SQLException {
 		Statement s= db.createStatement();
 		s.executeUpdate("create table usuarios ("
-				+ "id varchar(5) not null,"
+				+ "id varchar(5) not null primary key,"
 				+ "nif varchar(9) not null,"
 				+ "nombre varchar(45) not null,"
 				+ "apellidos varchar(45) not null,"
@@ -112,11 +115,14 @@ public class UsuariosDAO  implements OperacionesDAO {
 				+ "fechaAlta date,"
 				+ "claveAcceso varchar(16) not null,"				 
 				//enum opcional pues ya lo verifica java, esperar a la conversion para ver si complesa tenerlo como doble seguridad
-				+ "rol enum('INVITADO','NORMAL','ADMINISTRADOR') default (INVITADO),"
+				//+ "rol enum('INVITADO','NORMAL','ADMINISTRADOR') default (INVITADO)"
 				// alternativamente se puede dar por comprobado y ponerlo como un varchar
-				+ "primary key(id));"
+				+ "rol varchar(10) not null," 
+				+ "unique (nif),"
+				+ "unique (correo)"
+				+ ");"
 				);
-		
+
 	}
 
 	@Override
@@ -128,7 +134,7 @@ public class UsuariosDAO  implements OperacionesDAO {
 	@Override
 	public void alta(Object obj) throws DatosException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -140,7 +146,7 @@ public class UsuariosDAO  implements OperacionesDAO {
 	@Override
 	public void actualizar(Object obj) throws DatosException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -158,7 +164,7 @@ public class UsuariosDAO  implements OperacionesDAO {
 	@Override
 	public void borrarTodo() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/**
@@ -190,241 +196,136 @@ public class UsuariosDAO  implements OperacionesDAO {
 	 * @param obj - el Usuario a buscar.
 	 * @return - el Usuario encontrado o null si no encuentra.
 	 */
-	public Usuario obtener(Object obj) {
+	public Usuario obtener(Object obj) {		
+		assert obj != null;
 		return this.obtener(((Usuario) obj).getId());
 	}
-	
+
+	private void sincronizarBufferUsuarios() {
+		bufferUsuarios.clear();
+		for (int i = 0; i < tmUsuarios.getRowCount(); i++) {
+			try {
+				String id = (String) tmUsuarios.getValueAt(i,0);
+				Nif nif = new Nif((String) tmUsuarios.getValueAt(i,1));
+				Correo correo = new Correo((String) tmUsuarios.getValueAt(i,2));
+				String nombre = (String) tmUsuarios.getValueAt(i,3);
+				String apellidos = (String) tmUsuarios.getValueAt(i,4);
+				DireccionPostal domicilio = new DireccionPostal ((String) tmUsuarios.getValueAt(i,5),
+						(String) tmUsuarios.getValueAt(i,6),
+						(String) tmUsuarios.getValueAt(i,7),
+						(String) tmUsuarios.getValueAt(i,8));
+				Fecha fechaNacimiento = new Fecha ((java.sql.Date) tmUsuarios.getValueAt(i,9));
+				Fecha fechaAlta = new Fecha ((java.sql.Date) tmUsuarios.getValueAt(i,10));
+				ClaveAcceso claveAcceso = new ClaveAcceso();
+				claveAcceso.setTexto((String) tmUsuarios.getValueAt(i,11));
+				RolUsuario rol = null;
+				String rolString = ((String) tmUsuarios.getValueAt(i,12)) ;
+				rol = obtenerRol(rolString);
+				//genera y guarda objeto
+				bufferUsuarios.add(new Usuario (nif,nombre,apellidos,domicilio, correo,
+						fechaNacimiento, fechaAlta,claveAcceso, rol));
+			}
+			catch ( ModeloException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Obtiene el rol dado un string conel rol 
+	 * @param i
+	 * @return rol
+	 */
+	private RolUsuario obtenerRol(String rol) {
+		//RolUsuario rolUsr = null;
+		switch (rol) {		
+		case "NORMAL":
+			return  RolUsuario.NORMAL;
+		case "ADMINISTRADOR":
+			return  RolUsuario.ADMINISTRADOR;
+		}
+		return RolUsuario.INVITADO;
+	}
+
+	private void rellenarFilasModelo() {
+		Object[] datosFila = new Object[this.tmUsuarios.getColumnCount()];
+		// para cada fila en el ResultSet de la consulta
+		try {
+			while (rsUsuarios.next()) {
+				//se replica y add fila en el tableModel
+				for (int i = 0; i < datosFila.length; i++) {
+					datosFila[i] = this.rsUsuarios.getObject(i+1);
+				}
+				this.tmUsuarios.addRow(datosFila);
+			}		
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void borrarFilasModelo() {
+		// TODO Auto-generated method stub
+	}
+
+	private void establecerColumnasModelo()  {
+
+		try {
+			//obtiene metadatos
+			ResultSetMetaData metaDatos = this.rsUsuarios.getMetaData();
+			// num total de col
+			int numCol = metaDatos.getColumnCount();
+			// etq de cada col
+			Object[] etiquetas = new Object[numCol];
+			for (int i = 0; i < numCol; i++) {
+				etiquetas[i] = metaDatos.getColumnLabel(i+1);
+			}
+			//incorporar array de etq en el tableModel
+			this.tmUsuarios.setColumnIdentifiers(etiquetas);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Obtiene un usuario dado su idUsr, el correo o su nif.
 	 * @param id - el id de Usuario a buscar.
 	 * @return - el Usuario encontrado. 
 	 */
 	@Override
-	public Usuario obtener(String id) {
-		
-		
-		return null;			
+	public Usuario obtener(String idUsr) {
+		assert idUsr != null;
+
+		ejecutarConsulta(idUsr);
+		// set col and labels
+		establecerColumnasModelo();
+		// borrado previo de filas
+		borrarFilasModelo();
+		// volcado desde el resultSet
+		rellenarFilasModelo();
+		//actualiza buffer de objetos
+		sincronizarBufferUsuarios();
+		if (bufferUsuarios.size() > 0) {
+			return (Usuario) bufferUsuarios.get(0);
+		}
+		return null;		
 	}	
-//	
-//	/**
-//	 * Obtiene todos los usuarios almacenados.
-//	 * @return - la List con todos los usuarios.
-//	 */
-//	@Override
-//	public List <Usuario> obtenerTodos() {
-//		Query consulta = db.query();
-//		consulta.constrain(Usuario.class);
-//		return consulta.execute();
-//	}
-//
-//	/**
-//	 * Registro de nuevo usuario.
-//	 * @param usr 
-//	 * @throws DatosException 
-//	 */
-//	@Override
-//	public void alta(Object obj) throws DatosException {
-//		assert obj != null;
-//		Usuario usrNuevo = (Usuario) obj;
-//		if (obtener(usrNuevo.getId()) == null) {
-//			db.store(usrNuevo);  		
-//			registrarEquivalenciaId(usrNuevo);
-//			return;
-//		}
-//		else {
-//			if (obj.equals(usrNuevo)) {
-//				producirVariantesIdUsr(usrNuevo);
-//			}
-//			else {
-//				throw new DatosException("UsuariosDAO.alta:" + usrNuevo.getId() +" Ya existe.");
-//			}
-//		}
-//	}
-//
-//	/**
-//	 *  Si hay coincidencia de identificador hace 23 intentos de variar la última letra
-//	 *  procedente del NIF. Llama al generarVarianteIdUsr() de la clase Usuario.
-//	 * @param usrNuevo
-//	 * @throws DatosException
-//	 */
-//	private void producirVariantesIdUsr(Usuario usr) throws DatosException {
-//		// Coincidencia de id. Hay que generar variante
-//		int intentos = "ABCDEFGHJKLMNPQRSTUVWXYZ".length();
-//		do {
-//			// Generar variante y comprueba de nuevo.
-//			usr = new Usuario(usr);	
-//			if (obtener(usr.getId()) == null) {
-//				db.store(usr);  		
-//				registrarEquivalenciaId(usr);
-//				return;
-//			}
-//			intentos--;
-//		} while (intentos >= 0);
-//		throw new DatosException("UsuariosDAO.alta: imposible generar variante del " + usr.getId());
-//	}
-//
-//	/**
-//	 * Registra las equivalencias de nif y correo para un idUsr.
-//	 * @param usuario
-//	 */
-//	private void registrarEquivalenciaId(Usuario usuario) {
-//		//Obtiene mapa de equivalencias
-//		Map<String,String> mapaEquivalencias = obtenerMapaEquivalencias();
-//		//Registra equivalencias 
-//		mapaEquivalencias.put(usuario.getId(), usuario.getId());
-//		mapaEquivalencias.put(usuario.getNif().getTexto(), usuario.getId());
-//		mapaEquivalencias.put(usuario.getCorreo().getTexto(), usuario.getId());
-//		//actualiza datos
-//		db.store(mapaEquivalencias);	
-//	}
-//
-//	/**
-//	 * Elimina el objeto, dado el id utilizado para el almacenamiento.
-//	 * @param id - el identificador del objeto a eliminar.
-//	 * @return - el Objeto eliminado. 
-//	 * @throws DatosException - si no existe.
-//	 */
-//	@Override
-//	public Usuario baja(String id) throws DatosException {
-//		assert id != null;
-//		assert id != "";
-//		assert id != " ";
-//		Usuario usr = obtener(id);
-//		if (usr != null) {
-//			borrarEquivalenciaId(usr);
-//			db.delete(usr);
-//			return usr;
-//		}
-//		throw new DatosException("Baja: "+ id + " no existe.");
-//	} 
-//
-//	/**
-//	 *  Actualiza datos de un Usuario reemplazando el almacenado por el recibido. 
-//	 *  No admitirá cambios en el idUsr.
-//	 *	@param obj - Usuario con los cambios.
-//	 * @throws DatosException - si no existe.
-//	 */
-//	@Override
-//	public void actualizar(Object obj) throws DatosException  {
-//		assert obj != null;
-//		Usuario usrActualizado = (Usuario) obj;
-//		Usuario usrPrevio = (Usuario) obtener(usrActualizado.getId());
-//		if (usrPrevio != null) {
-//			try {
-//				cambiarEquivalenciaId(usrPrevio, usrActualizado);
-//				usrPrevio.setNif(usrActualizado.getNif());
-//				usrPrevio.setNombre(usrActualizado.getNombre());
-//				usrPrevio.setApellidos(usrActualizado.getApellidos());
-//				usrPrevio.setDomicilio(usrActualizado.getDomicilio());
-//				usrPrevio.setCorreo(usrActualizado.getCorreo());
-//				usrPrevio.setFechaNacimiento(usrActualizado.getFechaNacimiento());
-//				usrPrevio.setFechaAlta(usrActualizado.getFechaAlta());
-//				usrPrevio.setRol(usrActualizado.getRol());
-//				db.store(usrPrevio);
-//				return;
-//			}
-//			catch (ModeloException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		throw new DatosException("Actualizar: "+ usrActualizado.getId() + " no existe.");
-//	} 
-//
-//	/**
-//	 * Actualiza las equivalencias de nif y correo para un id de usuario.
-//	 * @param usrAntiguo - usuario con datos antiguos
-//	 * @param usrNuevo - usuario con datos nuevos
-//	 */
-//	private void cambiarEquivalenciaId(Usuario usrAntiguo, Usuario usrNuevo) {
-//		//Obtiene mapa de equivalencias
-//		Map<String,String> mapaEquivalencias = obtenerMapaEquivalencias();
-//		//Cambia equivalencias 
-//		mapaEquivalencias.replace(usrAntiguo.getId(), usrNuevo.getId().toUpperCase());
-//		mapaEquivalencias.replace(usrAntiguo.getNif().getTexto(), usrNuevo.getId().toUpperCase());
-//		mapaEquivalencias.replace(usrAntiguo.getCorreo().getTexto(), usrNuevo.getId().toUpperCase());
-//		//actualiza datos
-//		db.store(mapaEquivalencias);
-//	}
-//	
-//	/**
-//	 * Obtiene el listado de todos los usuarios almacenados.
-//	 * @return el texto con el volcado de datos.
-//	 */
-//	@Override
-//	public String listarDatos() {
-//		StringBuilder listado = new StringBuilder();
-//		for (Usuario usr: obtenerTodos()) {
-//			listado.append("\n" + usr);
-//		}
-//		return listado.toString();
-//	}
-//
-//	/**
-//	 * Obtiene el listado de todos los identificadores de usuario almacenados.
-//	 * @return el texto con el volcado de datos.
-//	 */
-//	@Override
-//	public String listarId() {
-//		StringBuilder listado = new StringBuilder();
-//		for (Usuario usr: obtenerTodos()) {
-//			if (usr != null) {
-//				listado.append("\n" + usr.getId());
-//			}
-//		}
-//		return listado.toString();
-//	}
-//
-//	/**
-//	 * Elimina todos los usuarios almacenados y regenera los predeterminados.
-//	 */
-//	@Override
-//	public void borrarTodo() {
-//		// Elimina cada uno de los obtenidos
-//		for (Usuario usr: obtenerTodos()) {
-//			db.delete(usr);
-//		}
-//		// Quita todas las equivalencias
-//		Map<String,String> mapaEquivalencias = obtenerMapaEquivalencias();
-//		mapaEquivalencias.clear();
-//		db.store(mapaEquivalencias);
-//		cargarPredeterminados();
-//	}
-//
-//	//GESTION equivalencias id
-//	/**
-//	 * Obtiene el idUsr usado internamente a partir de otro equivalente.
-//	 * @param id - la clave alternativa. 
-//	 * @return - El idUsr equivalente.
-//	 */
-//	public String obtenerEquivalencia(String id) {
-//		return obtenerMapaEquivalencias().get(id);
-//	}
-//
-//	/**
-//	 * Obtiene el mapa de equivalencias de id para idUsr.
-//	 * @return el Hashtable almacenado.
-//	 */
-//	private Map <String, String> obtenerMapaEquivalencias() {
-//		//Obtiene mapa de equivalencias de id de acceso
-//		Query consulta = db.query();
-//		consulta.constrain(Hashtable.class);
-//		ObjectSet <Hashtable <String,String>> result = consulta.execute();
-//		return result.get(0);	
-//	}
-//
-//	/**
-//	 * Elimina las equivalencias de nif y correo para un id de usuario.
-//	 * @param usuario - el usuario para eliminar sus equivalencias de id.
-//	 */
-//	private void borrarEquivalenciaId(Usuario usuario) {
-//		//Obtiene mapa de equivalencias
-//		Map<String,String> mapaEquivalencias = obtenerMapaEquivalencias();
-//		//Borra equivalencias 
-//		mapaEquivalencias.remove(usuario.getId());
-//		mapaEquivalencias.remove(usuario.getNif().getTexto());
-//		mapaEquivalencias.remove(usuario.getCorreo().getTexto());
-//		//actualiza datos
-//		db.store(mapaEquivalencias);	
-//	}
+
+	private void ejecutarConsulta(String idUsr) {
+		String id = "id";
+		if (idUsr.matches(Formato.PATRON_NIF)) {
+			id = "nif";
+		}
+		if (idUsr.matches(Formato.PATRON_CORREO1)) {
+			id = "correo";
+		}
+		try {
+			this.rsUsuarios = this.stUsuarios.executeQuery("select * from usuarios where "+ id +" = nif;");
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 } //class
